42285440cd749983598087fdb0da884fec4d87e2
Alexander Bloss <alexander.bloss@f-i-ts.de>
2019-12-06 15:31:09 +0100

    added properties for iam

--- pkg/apis/metal/types_controlplane.go
+++ pkg/apis/metal/types_controlplane.go
@@ -29,19 +29,69 @@ type ControlPlaneConfig struct {
 	// +optional
 	CloudControllerManager *CloudControllerManagerConfig
 
-	// TokenIssuer contains configuration settings for the token issuer.
-	TokenIssuer TokenIssuer
+	// IAMConfig contains the config for all AuthN/AuthZ related components
+	IAMConfig IAMConfig `json:"iamconfig" optional:"false"`
 }
 
 // CloudControllerManagerConfig contains configuration settings for the cloud-controller-manager.
 type CloudControllerManagerConfig struct {
-	gardenv1beta1.KubernetesConfig
+	gardenv1beta1.KubernetesConfig `json:",inline"`
 }
 
-// TokenIssuer for configuration of AuthNWebhook
-type TokenIssuer struct {
-	// IssuerURL the url of the issuer
-	IssuerURL string
-	// ClientId the id of the client
-	ClientID string
+// IAMConfig contains the config for all AuthN/AuthZ related components
+type IAMConfig struct {
+	IssuerConfig *IssuerConfig         `json:"issuerConfig,omitempty"`
+	IdmConfig    *IDMConfig            `json:"idmConfig,omitempty"`
+	GroupConfig  *NamespaceGroupConfig `json:"groupConfig,omitempty"`
+}
+
+// TokenIssuer contains configuration settings for the token issuer.
+type IssuerConfig struct {
+	Url      string `json:"url,omitempty"`
+	ClientId string `json:"clientId,omitempty"`
+}
+
+// IDMConfig contains config for the IDM-System that is used as directory for users and groups
+type IDMConfig struct {
+	Idmtype string `json:"idmtype,omitempty"`
+
+	ConnectorConfig *ConnectorConfig `json:"connectorConfig,omitempty"`
+}
+
+// NamespaceGroupConfig for group-rolebinding-controller
+type NamespaceGroupConfig struct {
+	// no action is taken or any namespace in this list
+	// kube-system,kube-public,kube-node-lease,default
+	ExcludedNamespaces string `json:"excludedNamespaces,omitempty"`
+	// for each element a RoleBinding is created in any Namespace - ClusterRoles are bound with this name
+	// admin,edit,view
+	ExpectedGroupsList string `json:"expectedGroupsList,omitempty"`
+	// Maximum length of namespace-part in clusterGroupname and therefore in the corresponding groupname in the directory.
+	// 20 chars für AD, given the FITS-naming-conventions
+	NamespaceMaxLength int `json:"namespaceMaxLength,omitempty"`
+	// The created RoleBindings will reference this group (from token).
+	// oidc:{{ .Namespace }}-{{ .Group }}
+	ClusterGroupnameTemplate string `json:"clusterGroupnameTemplate,omitempty"`
+	// The RoleBindings will created with this name.
+	// oidc-{{ .Namespace }}-{{ .Group }}
+	RoleBindingNameTemplate string `json:"roleBindingNameTemplate,omitempty"`
+}
+
+// ConnectorConfig optional config for the IDM Webhook - if it should be used to automatically create/delete groups/roles in the tenant IDM
+type ConnectorConfig struct {
+	IdmApiUrl            string `json:"idmApiUrl,omitempty"`
+	IdmApiUser           string `json:"idmApiUser,omitempty"`
+	IdmApiPassword       string `json:"idmApiPassword,omitempty"`
+	IdmSystemId          string `json:"idmSystemId,omitempty"`
+	IdmAccessCode        string `json:"idmAccessCode,omitempty"`
+	IdmCustomerId        string `json:"idmCustomerId,omitempty"`
+	IdmGroupOU           string `json:"idmGroupOU,omitempty"`
+	IdmGroupnameTemplate string `json:"idmGroupnameTemplate,omitempty"`
+	IdmDomainName        string `json:"idmDomainName,omitempty"`
+	IdmTenantPrefix      string `json:"idmTenantPrefix,omitempty"`
+	IdmSubmitter         string `json:"idmSubmitter,omitempty"`
+	IdmJobInfo           string `json:"idmJobInfo,omitempty"`
+	IdmReqSystem         string `json:"idmReqSystem,omitempty"`
+	IdmReqUser           string `json:"idmReqUser,omitempty"`
+	IdmReqEMail          string `json:"idmReqEMail,omitempty"`
 }
