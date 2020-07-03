42f96e93970a36565cd24e52ea95013c335f400d
Stefan Majer <stefan.majer@f-i-ts.de>
2020-07-03 12:12:53 +0200

    update to gardener 1.6

--- pkg/validator/serialization.go
+++ pkg/validator/serialization.go
@@ -2,13 +2,12 @@ package validator
 
 import (
 	"github.com/gardener/gardener/extensions/pkg/util"
-	"github.com/gardener/gardener/pkg/apis/core"
 	"github.com/metal-stack/gardener-extension-provider-metal/pkg/apis/metal"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
-func decodeControlPlaneConfig(decoder runtime.Decoder, cp *core.ProviderConfig, fldPath *field.Path) (*metal.ControlPlaneConfig, error) {
+func decodeControlPlaneConfig(decoder runtime.Decoder, cp *runtime.RawExtension, fldPath *field.Path) (*metal.ControlPlaneConfig, error) {
 	controlPlaneConfig := &metal.ControlPlaneConfig{}
 	if cp != nil && cp.Raw != nil {
 		if err := util.Decode(decoder, cp.Raw, controlPlaneConfig); err != nil {
@@ -19,7 +18,7 @@ func decodeControlPlaneConfig(decoder runtime.Decoder, cp *core.ProviderConfig, 
 	return controlPlaneConfig, nil
 }
 
-func decodeInfrastructureConfig(decoder runtime.Decoder, infra *core.ProviderConfig, fldPath *field.Path) (*metal.InfrastructureConfig, error) {
+func decodeInfrastructureConfig(decoder runtime.Decoder, infra *runtime.RawExtension, fldPath *field.Path) (*metal.InfrastructureConfig, error) {
 	infraConfig := &metal.InfrastructureConfig{}
 	if infra != nil && infra.Raw != nil {
 		if err := util.Decode(decoder, infra.Raw, infraConfig); err != nil {
