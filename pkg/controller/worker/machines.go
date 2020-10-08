1400f815c267d03961749c40f9a36593364172ac
Gerrit <tirreg91@googlemail.com>
2020-10-08 13:34:49 +0200

    Try out something else again.

--- pkg/controller/worker/machines.go
+++ pkg/controller/worker/machines.go
@@ -134,7 +134,7 @@ func (w *workerDelegate) DeployMachineClasses(ctx context.Context) error {
 	if ootDeployment {
 		// Delete any older version machine class CRs.
 		if err := w.cleanupOldMachineClasses(ctx, w.worker.Namespace, &metalv1alpha1.MetalMachineClassList{}, nil); err != nil {
-			w.logger.Info("unable to cleanup old metal machine classes by now, retrying later...", "error", err)
+			return fmt.Errorf("migration from MCM to OOT approach still pending: %v", err)
 		}
 	} else {
 		err := w.errorWhenAlreadyMigrated(ctx)
