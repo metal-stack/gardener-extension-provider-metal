3141b7d5797d7efbedcb7695dc28c2a9dc9678f3
Gerrit <Gerrit91@users.noreply.github.com>
2021-04-08 11:05:25 +0200

    Allow dynamic reconfiguration of prefixes on the firewall (#169)
    * Remove unnecessary duplicated backend calls.
    
    * Replace prefixes of machine allocation with prefixes from actual network.
    
    * Update firewall-controller to 1.0.2.
    
    * Generate.

--- pkg/controller/controlplane/valuesprovider.go
+++ pkg/controller/controlplane/valuesprovider.go
@@ -597,7 +597,7 @@ func (vp *valuesProvider) GetControlPlaneShootChartValues(ctx context.Context, c
 		nws[*n.ID] = n
 	}
 
-	values, err := vp.getControlPlaneShootChartValues(ctx, cp, cluster, nws, infrastructureConfig)
+	values, err := vp.getControlPlaneShootChartValues(ctx, cp, cpConfig, cluster, nws, infrastructureConfig, mclient)
 	if err != nil {
 		vp.logger.Error(err, "Error getting shoot control plane chart values")
 		return nil, err
@@ -621,15 +621,10 @@ func (vp *valuesProvider) GetControlPlaneShootChartValues(ctx context.Context, c
 }
 
 // getControlPlaneShootChartValues returns the values for the shoot control plane chart.
-func (vp *valuesProvider) getControlPlaneShootChartValues(ctx context.Context, cp *extensionsv1alpha1.ControlPlane, cluster *extensionscontroller.Cluster, nws networkMap, infrastructure *apismetal.InfrastructureConfig) (map[string]interface{}, error) {
+func (vp *valuesProvider) getControlPlaneShootChartValues(ctx context.Context, cp *extensionsv1alpha1.ControlPlane, cpConfig *apismetal.ControlPlaneConfig, cluster *extensionscontroller.Cluster, nws networkMap, infrastructure *apismetal.InfrastructureConfig, mclient *metalgo.Driver) (map[string]interface{}, error) {
 	namespace := cluster.ObjectMeta.Name
 
-	cpConfig, err := helper.ControlPlaneConfigFromControlPlane(cp)
-	if err != nil {
-		return nil, err
-	}
-
-	fwSpec, err := vp.getFirewallSpec(ctx, cp, cluster)
+	fwSpec, err := vp.getFirewallSpec(ctx, infrastructure, cluster, nws, mclient)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not assemble firewall values")
 	}
@@ -688,40 +683,10 @@ func (vp *valuesProvider) getControlPlaneShootChartValues(ctx context.Context, c
 	return values, nil
 }
 
-func (vp *valuesProvider) getFirewallSpec(ctx context.Context, cp *extensionsv1alpha1.ControlPlane, cluster *extensionscontroller.Cluster) (*firewallv1.FirewallSpec, error) {
+func (vp *valuesProvider) getFirewallSpec(ctx context.Context, infrastructureConfig *apismetal.InfrastructureConfig, cluster *extensionscontroller.Cluster, nws networkMap, mclient *metalgo.Driver) (*firewallv1.FirewallSpec, error) {
 	internalPrefixes := []string{}
 	if vp.controllerConfig.AccountingExporter.Enabled && vp.controllerConfig.AccountingExporter.NetworkTraffic.Enabled {
 		internalPrefixes = vp.controllerConfig.AccountingExporter.NetworkTraffic.InternalNetworks
-	}
-
-	infrastructureConfig := &apismetal.InfrastructureConfig{}
-	if _, _, err := vp.decoder.Decode(cluster.Shoot.Spec.Provider.InfrastructureConfig.Raw, nil, infrastructureConfig); err != nil {
-		return nil, errors.Wrapf(err, "could not decode providerConfig of infrastructure")
-	}
-
-	cpConfig, err := helper.ControlPlaneConfigFromControlPlane(cp)
-	if err != nil {
-		return nil, err
-	}
-
-	cloudProfileConfig, err := helper.CloudProfileConfigFromCluster(cluster)
-	if err != nil {
-		return nil, err
-	}
-
-	metalControlPlane, _, err := helper.FindMetalControlPlane(cloudProfileConfig, infrastructureConfig.PartitionID)
-	if err != nil {
-		return nil, err
-	}
-
-	cpConfig.IAMConfig, err = helper.MergeIAMConfig(metalControlPlane.IAMConfig, cpConfig.IAMConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	mclient, err := metalclient.NewClient(ctx, vp.client, metalControlPlane.Endpoint, &cp.Spec.SecretRef)
-	if err != nil {
-		return nil, err
 	}
 
 	rateLimits := []firewallv1.RateLimit{}
@@ -753,6 +718,24 @@ func (vp *valuesProvider) getFirewallSpec(ctx context.Context, cp *extensionsv1a
 	firewall := *firewalls[0]
 	firewallNetworks := []firewallv1.FirewallNetwork{}
 	for _, n := range firewall.Allocation.Networks {
+		if n.Networkid == nil {
+			continue
+		}
+		n := n
+
+		// prefixes in the firewall machine allocation are just a snapshot when the firewall was created.
+		// -> when changing prefixes in the referenced network the firewall does not know about any prefix changes.
+		//
+		// we replace the prefixes from the snapshot with the actual prefixes that are currently attached to the network.
+		// this allows dynamic prefix reconfiguration of the firewall.
+		prefixes := n.Prefixes
+		networkRef, ok := nws[*n.Networkid]
+		if !ok {
+			vp.logger.Info("network in firewall allocation does not exist anymore")
+		} else {
+			prefixes = networkRef.Prefixes
+		}
+
 		firewallNetworks = append(firewallNetworks, firewallv1.FirewallNetwork{
 			Asn:                 n.Asn,
 			Destinationprefixes: n.Destinationprefixes,
@@ -760,7 +743,7 @@ func (vp *valuesProvider) getFirewallSpec(ctx context.Context, cp *extensionsv1a
 			Nat:                 n.Nat,
 			Networkid:           n.Networkid,
 			Networktype:         n.Networktype,
-			Prefixes:            n.Prefixes,
+			Prefixes:            prefixes,
 			Vrf:                 n.Vrf,
 		})
 	}
