f234d289877afa9564c4009b4c2c982b688daf82
mreiger <michael@rauschpfeife.net>
2021-05-28 16:55:28 +0200

    Copy healthCheckNodePort over from the old service so that the new service can be applied

--- pkg/webhook/controlplaneexposure/ensurer.go
+++ pkg/webhook/controlplaneexposure/ensurer.go
@@ -48,11 +48,18 @@ func (e *ensurer) InjectClient(client client.Client) error {
 // EnsureKubeAPIServerService ensures that the kube-apiserver service conforms to the provider requirements.
 func (e *ensurer) EnsureKubeAPIServerService(ctx context.Context, gctx gcontext.GardenContext, new, old *corev1.Service) error {
 	cluster, _ := gctx.GetCluster(ctx)
-	e.logger.Info("kube-apiserver service externalTrafficPolicy", "old", old.Spec.ExternalTrafficPolicy, "new", new.Spec.ExternalTrafficPolicy)
+
+	e.logger.Info("kube-apiserver service externalTrafficPolicy of", "shoot", cluster.ObjectMeta.Name, "old", old.Spec.ExternalTrafficPolicy, "new", new.Spec.ExternalTrafficPolicy)
+	e.logger.Info("kube-apiserver service healthCheckNodePort of", "shoot", cluster.ObjectMeta.Name, "old", old.Spec.HealthCheckNodePort, "new", new.Spec.HealthCheckNodePort)
+	// We ensure that the externalTrafficPolicy of the kube-apiserver service is Local, and that the healthCheckNodePort is preserved.
 	if new.Spec.ExternalTrafficPolicy != corev1.ServiceExternalTrafficPolicyType("Local") {
 		e.logger.Info("kube-apiserver service externalTrafficPolicy of", "shoot", cluster.ObjectMeta.Name, "now", new.Spec.ExternalTrafficPolicy, "changing to", corev1.ServiceExternalTrafficPolicyType("Local"))
 		new.Spec.ExternalTrafficPolicy = corev1.ServiceExternalTrafficPolicyType("Local")
 	}
+	if new.Spec.HealthCheckNodePort == 0 {
+		new.Spec.HealthCheckNodePort = old.Spec.HealthCheckNodePort
+	}
+
 	return nil
 }
 
