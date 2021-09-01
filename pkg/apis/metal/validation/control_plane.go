f9cdcac39b575993ae9c8eba2e831639f552e5a6
mreiger <michael@rauschpfeife.net>
2021-09-01 14:53:07 +0200

    Validate audit featureGates

--- pkg/apis/metal/validation/control_plane.go
+++ pkg/apis/metal/validation/control_plane.go
@@ -11,6 +11,15 @@ import (
 func ValidateControlPlaneConfig(controlPlaneConfig *apismetal.ControlPlaneConfig, cloudProfile *gardencorev1beta1.CloudProfile, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
+	allErrs = append(allErrs, validateIAMConfig(controlPlaneConfig, fldPath)...)
+	allErrs = append(allErrs, validateFeatureGates(controlPlaneConfig, fldPath)...)
+
+	return allErrs
+}
+
+func validateIAMConfig(controlPlaneConfig *apismetal.ControlPlaneConfig, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
 	iam := controlPlaneConfig.IAMConfig
 	iamPath := fldPath.Child("iamconfig")
 	if iam == nil {
@@ -36,6 +45,28 @@ func ValidateControlPlaneConfig(controlPlaneConfig *apismetal.ControlPlaneConfig
 	if groupConfig != nil {
 		if groupConfig.NamespaceMaxLength <= 0 {
 			allErrs = append(allErrs, field.Required(groupConfigPath.Child("namespaceMaxLength"), "namespaceMaxLength must be a positive integer"))
+		}
+	}
+
+	return allErrs
+}
+
+func validateFeatureGates(controlPlaneConfig *apismetal.ControlPlaneConfig, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	fgPath := fldPath.Child("featureGates")
+
+	if &controlPlaneConfig.FeatureGates != nil {
+	}
+	clusterAudit := controlPlaneConfig.FeatureGates.ClusterAudit
+	clusterAuditPath := fgPath.Child("clusterAudit")
+
+	auditToSplunk := controlPlaneConfig.FeatureGates.AuditToSplunk
+	auditToSplunkPath := fgPath.Child("auditToSplunk")
+
+	if auditToSplunk != nil && *auditToSplunk {
+		if clusterAudit == nil || !*clusterAudit {
+			allErrs = append(allErrs, field.Invalid(auditToSplunkPath, clusterAuditPath, "auditToSplunk is set but clusterAudit is not set"))
 		}
 	}
 
