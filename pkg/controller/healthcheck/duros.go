eefd65e9b49f81ee827885b0532de21bf9ce5e19
Gerrit <info@gerritschwerthelm.de>
2021-09-13 09:49:07 +0200

    Implement health check for duros resource.

---
+++ pkg/controller/healthcheck/duros.go
@@ -0,0 +1,86 @@
+package healthcheck
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/go-logr/logr"
+	durosv1 "github.com/metal-stack/duros-controller/api/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// DurosHealthChecker contains all the information for the Duros HealthCheck
+type DurosHealthChecker struct {
+	logger            logr.Logger
+	seedClient        client.Client
+	durosResourceName string
+}
+
+// CheckDuros is a healthCheck function to check Duross
+func CheckDuros(durosResourceName string) healthcheck.HealthCheck {
+	return &DurosHealthChecker{
+		durosResourceName: durosResourceName,
+	}
+}
+
+// InjectSeedClient injects the seed client
+func (healthChecker *DurosHealthChecker) InjectSeedClient(seedClient client.Client) {
+	healthChecker.seedClient = seedClient
+}
+
+// SetLoggerSuffix injects the logger
+func (healthChecker *DurosHealthChecker) SetLoggerSuffix(provider, extension string) {
+	healthChecker.logger = log.Log.WithName(fmt.Sprintf("%s-%s-healthcheck-duros", provider, extension))
+}
+
+// DeepCopy clones the healthCheck struct by making a copy and returning the pointer to that new copy
+func (healthChecker *DurosHealthChecker) DeepCopy() healthcheck.HealthCheck {
+	copy := *healthChecker
+	return &copy
+}
+
+// Check executes the health check
+func (healthChecker *DurosHealthChecker) Check(ctx context.Context, request types.NamespacedName) (*healthcheck.SingleCheckResult, error) {
+	duros := &durosv1.Duros{}
+
+	if err := healthChecker.seedClient.Get(ctx, client.ObjectKey{Namespace: request.Namespace, Name: healthChecker.durosResourceName}, duros); err != nil {
+		if apierrors.IsNotFound(err) {
+			// we skip the health check when there is no duros resource deployed
+			return &healthcheck.SingleCheckResult{
+				Status: gardencorev1beta1.ConditionTrue,
+			}, nil
+		}
+
+		err := fmt.Errorf("check duros resource failed. Unable to retrieve duros resource '%s' in namespace '%s': %v", healthChecker.durosResourceName, request.Namespace, err)
+		healthChecker.logger.Error(err, "Health check failed")
+		return nil, err
+	}
+	if isHealthy, reason, err := DurosIsHealthy(duros); !isHealthy {
+		healthChecker.logger.Error(err, "Health check failed")
+		return &healthcheck.SingleCheckResult{
+			Status: gardencorev1beta1.ConditionFalse,
+			Detail: err.Error(),
+			Reason: *reason,
+		}, nil
+	}
+
+	return &healthcheck.SingleCheckResult{
+		Status: gardencorev1beta1.ConditionTrue,
+	}, nil
+}
+
+func DurosIsHealthy(duros *durosv1.Duros) (bool, *string, error) {
+	// TODO: Implement check
+	if duros == nil {
+		reason := "DurosUnhealthy"
+		err := fmt.Errorf("duros resource %s in namespace %s is unhealthy: %v", duros.Name, duros.Namespace, fmt.Errorf("reason"))
+		return false, &reason, err
+	}
+	return true, nil, nil
+}
