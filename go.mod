934ca0243c09f68fce06a6bbd9804146b74f82e6
Gerrit <info@gerritschwerthelm.de>
2023-01-31 18:12:21 +0100

    Use label constants.

--- go.mod
+++ go.mod
@@ -98,6 +98,7 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/metal-stack/security v0.6.6 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/moby/spdystream v0.2.0 // indirect
--- pkg/apis/metal/helper/secrets.go
+++ pkg/apis/metal/helper/secrets.go
@@ -8,38 +8,39 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	secretsmanager "github.com/gardener/gardener/pkg/utils/secrets/manager"
 )
 
 func GetLatestSSHSecret(ctx context.Context, c client.Client, namespace string) (*corev1.Secret, error) {
 	secretList := &corev1.SecretList{}
 	if err := c.List(ctx, secretList, client.InNamespace(namespace), client.MatchingLabels{
 		// TODO: migrate to secretsmanager constants on g/g v1.45
-		"managed-by":       "secrets-manager",
-		"manager-identity": "gardenlet",
-		"name":             "ssh-keypair",
+		secretsmanager.LabelKeyManagedBy:       secretsmanager.LabelValueSecretsManager,
+		secretsmanager.LabelKeyManagerIdentity: "gardenlet",
+		secretsmanager.LabelKeyName:            "ssh-keypair",
 	}); err != nil {
 		return nil, err
 	}
 
-	return GetLatestIssuedSecret(secretList.Items)
+	return getLatestIssuedSecret(secretList.Items)
 }
 
 func GetLatestCABundle(ctx context.Context, c client.Client, namespace string) (*corev1.Secret, error) {
 	secretList := &corev1.SecretList{}
 	if err := c.List(ctx, secretList, client.InNamespace(namespace), client.MatchingLabels{
-		// TODO: migrate to secretsmanager constants on g/g v1.45
-		"managed-by":       "secrets-manager",
-		"manager-identity": "gardenlet",
-		"name":             "ca-bundle",
+		secretsmanager.LabelKeyManagedBy:       secretsmanager.LabelValueSecretsManager,
+		secretsmanager.LabelKeyManagerIdentity: "gardenlet",
+		secretsmanager.LabelKeyName:            "ca-bundle",
 	}); err != nil {
 		return nil, err
 	}
 
-	return GetLatestIssuedSecret(secretList.Items)
+	return getLatestIssuedSecret(secretList.Items)
 }
 
 // getLatestIssuedSecret returns the secret with the "issued-at-time" label that represents the latest point in time
-func GetLatestIssuedSecret(secrets []corev1.Secret) (*corev1.Secret, error) {
+func getLatestIssuedSecret(secrets []corev1.Secret) (*corev1.Secret, error) {
 	if len(secrets) == 0 {
 		return nil, fmt.Errorf("no secret found")
 	}
@@ -49,7 +50,7 @@ func GetLatestIssuedSecret(secrets []corev1.Secret) (*corev1.Secret, error) {
 	for i := 0; i < len(secrets); i++ {
 		// if some of the secrets have no "issued-at-time" label
 		// we have a problem since this is the source of truth
-		issuedAt, ok := secrets[i].Labels["issued-at-time"]
+		issuedAt, ok := secrets[i].Labels[secretsmanager.LabelKeyIssuedAtTime]
 		if !ok {
 			return nil, fmt.Errorf("secret with no issues-at-time label: %s", secrets[i].Name)
 		}
