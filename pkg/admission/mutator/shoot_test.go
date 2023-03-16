7662d385b7a903c341cc422cd2a9304e3a0a4d89
Gerrit <info@gerritschwerthelm.de>
2023-03-16 19:20:23 +0100

    Fix test temporarily.

--- pkg/admission/mutator/shoot_test.go
+++ pkg/admission/mutator/shoot_test.go
@@ -28,7 +28,7 @@ func Test_shoot_Mutate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &shoot{
+			s := &mutator{
 				client:  tt.fields.client,
 				decoder: tt.fields.decoder,
 			}
