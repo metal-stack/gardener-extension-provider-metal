c3a06f55dbf1c53071e768d20b4758ae553ea451
Gerrit <info@gerritschwerthelm.de>
2023-04-11 16:14:54 +0200

    Bring back codegen.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -12,13 +12,13 @@ rm -f $GOPATH/bin/*-gen
 
 PROJECT_ROOT=$(dirname $0)/..
 
-# bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
-#   deepcopy,defaulter \
-#   github.com/metal-stack/gardener-extension-provider-metal/pkg/client \
-#   github.com/metal-stack/gardener-extension-provider-metal/pkg/apis \
-#   github.com/metal-stack/gardener-extension-provider-metal/pkg/apis \
-#   "metal:v1alpha1" \
-#   --go-header-file "${PROJECT_ROOT}/hack/boilerplate.txt"
+bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
+  deepcopy,defaulter \
+  github.com/metal-stack/gardener-extension-provider-metal/pkg/client \
+  github.com/metal-stack/gardener-extension-provider-metal/pkg/apis \
+  github.com/metal-stack/gardener-extension-provider-metal/pkg/apis \
+  "metal:v1alpha1" \
+  --go-header-file "${PROJECT_ROOT}/hack/boilerplate.txt"
 
 bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
   conversion \
