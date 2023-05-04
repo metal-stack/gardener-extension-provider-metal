c4dc45fdebe3d354920c6a75f30907cbdf937823
Gerrit <info@gerritschwerthelm.de>
2023-05-04 12:00:44 +0200

    Create dedicated ca for control plane.

--- pkg/controller/controlplane/valuesprovider.go
+++ pkg/controller/controlplane/valuesprovider.go
@@ -71,12 +71,18 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+const (
+	caNameControlPlane                  = "ca-" + metal.Name + "-controlplane"
+	cloudControllerManagerServerName    = metal.CloudControllerManagerDeploymentName + "-server"
+	firewallControllerManagerServerName = metal.FirewallControllerManagerDeploymentName + "-server"
+)
+
 func secretConfigsFunc(namespace string) []extensionssecretsmanager.SecretConfigWithOptions {
 	return []extensionssecretsmanager.SecretConfigWithOptions{
 		{
 			Config: &secrets.CertificateSecretConfig{
-				Name:       v1alpha1constants.SecretNameCACluster,
-				CommonName: "kubernetes",
+				Name:       caNameControlPlane,
+				CommonName: caNameControlPlane,
 				CertType:   secrets.CACert,
 			},
 			Options: []secretsmanager.GenerateOption{secretsmanager.Persist()},
@@ -89,7 +95,7 @@ func secretConfigsFunc(namespace string) []extensionssecretsmanager.SecretConfig
 				CertType:                    secrets.ServerCert,
 				SkipPublishingCACertificate: true,
 			},
-			Options: []secretsmanager.GenerateOption{secretsmanager.SignedByCA(v1alpha1constants.SecretNameCACluster, secretsmanager.UseCurrentCA)},
+			Options: []secretsmanager.GenerateOption{secretsmanager.SignedByCA(caNameControlPlane, secretsmanager.UseCurrentCA)},
 		},
 		{
 			Config: &secrets.CertificateSecretConfig{
@@ -101,7 +107,7 @@ func secretConfigsFunc(namespace string) []extensionssecretsmanager.SecretConfig
 			},
 			// use current CA for signing server cert to prevent mismatches when dropping the old CA from the webhook
 			// config in phase Completing
-			Options: []secretsmanager.GenerateOption{secretsmanager.SignedByCA(v1alpha1constants.SecretNameCACluster, secretsmanager.UseCurrentCA)},
+			Options: []secretsmanager.GenerateOption{secretsmanager.SignedByCA(caNameControlPlane, secretsmanager.UseCurrentCA)},
 		},
 	}
 }
