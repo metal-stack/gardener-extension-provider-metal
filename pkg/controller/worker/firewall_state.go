369c1f595a3c28e26e088769a628a41cffdd1ad6
Gerrit <info@gerritschwerthelm.de>
2023-06-20 16:06:55 +0200

    Use sigs yaml.

--- pkg/controller/worker/firewall_state.go
+++ pkg/controller/worker/firewall_state.go
@@ -6,13 +6,16 @@ import (
 	"fmt"
 
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+
 	fcmv2 "github.com/metal-stack/firewall-controller-manager/api/v2"
-	"gopkg.in/yaml.v2"
+
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/yaml"
 )
 
 // InfrastructureState represents the last known State of an Infrastructure resource.
