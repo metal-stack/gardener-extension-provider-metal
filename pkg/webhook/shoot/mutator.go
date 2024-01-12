1e95a6e26e1e2fe7243fb657260bae639d2912de
Stefan Majer <stefan.majer@f-i-ts.de>
2024-01-12 13:24:34 +0100

    Simplify code, add some simple test

--- pkg/webhook/shoot/mutator.go
+++ pkg/webhook/shoot/mutator.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/gardener/gardener/extensions/pkg/util"
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
-	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/gardener/gardener/pkg/component/extensions/operatingsystemconfig/downloader"
 	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
@@ -38,7 +37,7 @@ type mutator struct {
 }
 
 // NewMutator creates a new Mutator that mutates resources in the shoot cluster.
-func NewMutator() extensionswebhook.MutatorWithShootClient {
+func NewMutator() extensionswebhook.Mutator {
 	return &mutator{
 		logger: log.Log.WithName("shoot-mutator"),
 	}
@@ -46,7 +45,6 @@ func NewMutator() extensionswebhook.MutatorWithShootClient {
 
 // InjectScheme injects the given scheme into the validator.
 func (s *mutator) InjectScheme(scheme *runtime.Scheme) error {
-	gardencorev1beta1.AddToScheme(scheme)
 	s.decoder = serializer.NewCodecFactory(scheme).UniversalDecoder()
 	return nil
 }
@@ -57,7 +55,7 @@ func (s *mutator) InjectClient(client client.Client) error {
 	return nil
 }
 
-func (m *mutator) Mutate(ctx context.Context, new, _ client.Object, shootClient client.Client) error {
+func (m *mutator) Mutate(ctx context.Context, new, _ client.Object) error {
 	acc, err := meta.Accessor(new)
 	if err != nil {
 		return fmt.Errorf("could not create accessor during webhook %w", err)
@@ -78,9 +76,7 @@ func (m *mutator) Mutate(ctx context.Context, new, _ client.Object, shootClient 
 		// TODO: remove this once gardener-node-agent is in use
 		// the purpose of this hack is to enable the cloud-config-downloader to pull the hyperkube image from
 		// a registry mirror in case this shoot cluster is configured with networkaccesstype restricted/forbidden
-		// FIXME only for isolated clusters
-		extensionswebhook.LogMutation(logger, x.Kind, x.Namespace, x.Name)
-		err = m.mutateCloudConfigDownloaderHyperkubeImage(ctx, x, shootClient)
+		err = m.mutateCloudConfigDownloaderHyperkubeImage(ctx, x)
 		if err != nil {
 			// FIXME is this correct
 			m.logger.Error(err, "mutation did not work")
@@ -113,28 +109,19 @@ func (m *mutator) mutateVPNShootDeployment(_ context.Context, deployment *appsv1
 	return nil
 }
 
-// var remoteAddrContextKey = struct{}{}
-
 const (
 	gardenerRegistry = "eu.gcr.io"
 	hyperkubeImage   = "/gardener-project/hyperkube"
 )
 
-func (m *mutator) mutateCloudConfigDownloaderHyperkubeImage(ctx context.Context, secret *corev1.Secret, shootClient client.Client) error {
+func (m *mutator) mutateCloudConfigDownloaderHyperkubeImage(ctx context.Context, secret *corev1.Secret) error {
 	if secret.Labels["gardener.cloud/role"] != "cloud-config" {
 		return nil
 	}
 
-	shootName := ""
-
-	for k, v := range secret.Annotations {
-		if k == "resources.gardener.cloud/origin" {
-			shootName = strings.Split(strings.Split(v, ":")[1], "/")[0]
-			break
-		}
-	}
-	if len(shootName) == 0 {
-		return fmt.Errorf("could not find shoot name for webhook request")
+	shootName, err := extractShootNameFromSecret(secret)
+	if err != nil {
+		return err
 	}
 
 	cluster := &extensionsv1alpha1.Cluster{}
@@ -148,7 +135,7 @@ func (m *mutator) mutateCloudConfigDownloaderHyperkubeImage(ctx context.Context,
 	}
 
 	if len(shoot.Spec.Provider.Workers) == 0 {
-		m.logger.Info("workerless shoot, nothing to do here")
+		m.logger.Info("workerless shoot, nothing to do here", "shoot", shootName)
 		return nil
 	}
 
@@ -160,7 +147,7 @@ func (m *mutator) mutateCloudConfigDownloaderHyperkubeImage(ctx context.Context,
 	}
 
 	if controlPlaneConfig.NetworkAccessType == nil || *controlPlaneConfig.NetworkAccessType == metal.NetworkAccessBaseline {
-		m.logger.Info("this shoot does not have networkaccesstype restricted or forbidden specified, nothing to do here")
+		m.logger.Info("this shoot does not have networkaccesstype restricted or forbidden specified, nothing to do here", "shoot", shootName)
 		return nil
 	}
 
@@ -174,13 +161,12 @@ func (m *mutator) mutateCloudConfigDownloaderHyperkubeImage(ctx context.Context,
 		}
 	}
 	if len(networkIsolation.RegistryMirrors) == 0 {
-		m.logger.Info("no registry mirrors specified in this shoot, nothing to do here")
+		m.logger.Info("no registry mirrors specified in this shoot, nothing to do here", "shoot", shootName)
 		return nil
 	}
 
 	var destinationRegistry string
 	for _, registry := range networkIsolation.RegistryMirrors {
-		// FIXME where to store this registry constant of the hyperkube image
 		if slices.Contains(registry.MirrorOf, gardenerRegistry) {
 			parsed, err := url.Parse(registry.Endpoint)
 			if err != nil {
@@ -192,9 +178,11 @@ func (m *mutator) mutateCloudConfigDownloaderHyperkubeImage(ctx context.Context,
 	}
 	if destinationRegistry == "" {
 		err := errors.New("no matching destination registry detected for the hyperkube image")
-		m.logger.Error(err, "please check the networkisolation configuration")
+		m.logger.Error(err, "please check the networkisolation configuration", "shoot", shootName)
 		return err
 	}
+
+	m.logger.Info("mutate secret", "shoot", shootName, "secret", secret.Name)
 
 	raw, ok := secret.Data[downloader.DataKeyScript]
 	if ok {
@@ -204,4 +192,25 @@ func (m *mutator) mutateCloudConfigDownloaderHyperkubeImage(ctx context.Context,
 		secret.Annotations[downloader.AnnotationKeyChecksum] = utils.ComputeChecksum(newScript)
 	}
 	return nil
+}
+
+func extractShootNameFromSecret(secret *corev1.Secret) (string, error) {
+	v, ok := secret.Annotations["resources.gardener.cloud/origin"]
+	if !ok {
+		return "", fmt.Errorf("no matching annotation found to identify the shoot namespace")
+	}
+
+	// resources.gardener.cloud/origin: shoot--test--fra-equ01-8fef639c-bbe4-4c6f-9656-617dc4a4efd8-gardener-soil-test:shoot--pjb9j2--forbidden/shoot-cloud-config-execution
+	_, firstpart, found := strings.Cut(v, ":")
+	if !found {
+		return "", fmt.Errorf("no matching content found in origin annotation to get shoot namespace")
+	}
+	shootName, _, found := strings.Cut(firstpart, "/")
+	if !found {
+		return "", fmt.Errorf("no matching content found in origin annotation to get shoot namespace")
+	}
+	if len(shootName) == 0 {
+		return "", fmt.Errorf("could not find shoot name for webhook request")
+	}
+	return shootName, nil
 }
