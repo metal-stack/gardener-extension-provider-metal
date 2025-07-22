56a5ad50b1f7acd56a82dae240adac106e2686a2
Gerrit <Gerrit91@users.noreply.github.com>
2025-07-22 09:15:32 +0200

    Allow setting explicit hash. (#462)

--- pkg/controller/worker/machines.go
+++ pkg/controller/worker/machines.go
@@ -24,6 +24,9 @@ import (
 
 const (
 	MutatingWebhookObjectSelectorLabel = "gardener-shoot-namespace"
+
+	keepWorkerHashAnnotation           = "cluster.metal-stack.io/keep-worker-hash"
+	explicitWorkerHashAnnotationPrefix = "cluster.metal-stack.io/use-worker-hash-"
 )
 
 // MachineClassKind yields the name of the machine class.
@@ -77,8 +80,13 @@ func (w *workerDelegate) generateMachineConfig(ctx context.Context) error {
 	}
 
 	keepHash := func(deploymentName string) (string, bool, error) {
-		if _, ok := w.cluster.Shoot.Annotations["cluster.metal-stack.io/keep-worker-hash"]; !ok {
+		if _, ok := w.cluster.Shoot.Annotations[keepWorkerHashAnnotation]; !ok {
 			return "", false, nil
+		}
+
+		groupName, found := strings.CutPrefix(deploymentName, w.cluster.ObjectMeta.Name+"-")
+		if !found {
+			return "", false, fmt.Errorf("unable to extract worker group name from deployment name %q", deploymentName)
 		}
 
 		classes := &machinev1alpha1.MachineClassList{}
@@ -101,8 +109,21 @@ func (w *workerDelegate) generateMachineConfig(ctx context.Context) error {
 		}
 
 		if hash == "" {
-			w.logger.Info("no machine classes found, allow creation of a new one", "name", deploymentName)
+			if explicitHash, ok := w.cluster.Shoot.Annotations[explicitWorkerHashAnnotation(groupName)]; ok {
+				w.logger.Info("no existing machine class found, using explicit worker hash for new machine deployment", "group-name", groupName, "hash", explicitHash)
+				return explicitHash, true, nil
+			}
+
+			w.logger.Info("no existing machine class found, allow creation of a new one", "name", deploymentName)
 			return "", false, nil
+		}
+
+		if explicitHash, ok := w.cluster.Shoot.Annotations[explicitWorkerHashAnnotation(groupName)]; ok {
+			w.logger.Info("explicit worker hash set for worker group", "group-name", groupName, "hash", explicitHash)
+
+			if hash != explicitHash {
+				return "", false, fmt.Errorf("unable to use explicit hash which differs from hash that was figured out from existing machine classes")
+			}
 		}
 
 		return hash, true, nil
@@ -296,4 +317,8 @@ func (w *workerDelegate) networkIsolationIfEnabled() *apismetal.NetworkIsolation
 	}
 
 	return w.additionalData.partition.NetworkIsolation
+}
+
+func explicitWorkerHashAnnotation(groupName string) string {
+	return explicitWorkerHashAnnotationPrefix + groupName
 }
